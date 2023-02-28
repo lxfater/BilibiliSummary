4e5b5647928479130f78b362b3565b51cddf2a61
zhuqixiang <595192167@qq.com>
2023-02-28 10:32:01 +0800

    fix(fix URI malformed):

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,7 +1,7 @@
 
 export function getSummaryPrompt(title: string, transcript: string, byteLimit: number) {
   const truncatedTranscript = limitTranscriptByteLength(transcript, byteLimit);
-  return `标题: "${title.replace(/\n+/g, " ").trim()}"\n视频字幕: "${truncatedTranscript.replace(/\n+/g, " ").trim()}"\n请用中文进行视频总结:`;
+  return `标题: "${title.replace(/\n+/g, " ").trim()}"\n字幕: "${truncatedTranscript.replace(/\n+/g, " ").trim()}"\n中文总结:`;
 }
 
 export function limitTranscriptByteLength(str: string, byteLimit: number) {
@@ -9,9 +9,8 @@ export function limitTranscriptByteLength(str: string, byteLimit: number) {
   const byteLength = utf8str.length;
   if (byteLength > byteLimit) {
     const ratio = byteLimit / byteLength;
-    const truncatedLength = Math.floor(utf8str.length * ratio);
-    const truncatedStr = utf8str.substring(0, truncatedLength);
-    return decodeURIComponent(escape(truncatedStr));
+    const newStr = str.substring(0, Math.floor(str.length * ratio));
+    return newStr;
   }
   return str;
 }
@@ -19,7 +18,7 @@ function filterHalfRandomly<T>(arr: T[]): T[] {
   const filteredArr: T[] = [];
   const halfLength = Math.floor(arr.length / 2);
   const indicesToFilter = new Set<number>();
-  
+
   // 随机生成要过滤掉的元素的下标
   while (indicesToFilter.size < halfLength) {
     const index = Math.floor(Math.random() * arr.length);
@@ -27,14 +26,14 @@ function filterHalfRandomly<T>(arr: T[]): T[] {
       indicesToFilter.add(index);
     }
   }
-  
+
   // 过滤掉要过滤的元素
   for (let i = 0; i < arr.length; i++) {
     if (!indicesToFilter.has(i)) {
       filteredArr.push(arr[i]);
     }
   }
-  
+
   return filteredArr;
 }
 function getByteLength(text: string) {
@@ -49,7 +48,7 @@ type SubtitleItem = {
   text: string;
   index: number;
 }
-export function getSmallSizeTranscripts(newTextData: SubtitleItem[], oldTextData: SubtitleItem[], byteLimit: number) {
+export function getSmallSizeTranscripts(newTextData: SubtitleItem[], oldTextData: SubtitleItem[], byteLimit: number): string {
   const text = newTextData.sort((a, b) => a.index - b.index).map(t => t.text).join(" ");
   const byteLength = getByteLength(text);
 
@@ -61,13 +60,13 @@ export function getSmallSizeTranscripts(newTextData: SubtitleItem[], oldTextData
   let resultData = newTextData.slice();
   let resultText = text;
   let lastByteLength = byteLength;
-  
+
   for (let i = 0; i < oldTextData.length; i++) {
     const obj = oldTextData[i];
     if (itemInIt(newTextData, obj.text)) {
       continue;
     }
-  
+
     const nextTextByteLength = getByteLength(obj.text);
     const isOverLimit = lastByteLength + nextTextByteLength > byteLimit;
     if (isOverLimit) {
